848d0a7d28c5dce42ec87f2f173832ab71bbb605
AnifInnab <anifinnab13@gmail.com>
2015-04-14 03:18:20 +0200

    S

--- Rpi_logger.py
+++ Rpi_logger.py
@@ -4,8 +4,6 @@ import obd_sensors
 obd = OBD_IO.OBDPort("/dev/pts/2", 1, 5)
 
 for i in range (20):
-    obd.send_command("010c")
-    rpm = obd_sensors.rpm(obd.interpret_result(obd.get_result()))
-    print(rpm)
+    print(obd.get_sensor_value("010c"))
 
 print("Sucess!")
