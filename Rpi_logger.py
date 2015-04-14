b4b1c9335824be9be752e26d56c62f0a9fd02cff
AnifInnab <anifinnab13@gmail.com>
2015-04-14 03:35:51 +0200

    ptw21

--- Rpi_logger.py
+++ Rpi_logger.py
@@ -1,7 +1,7 @@
 import OBD_IO
 import obd_sensors
 
-obd = OBD_IO.OBDPort("/dev/pts/2", 1, 5)
+obd = OBD_IO.OBDPort("/dev/pts/1", 1, 5)
 
 for i in range (20):
     print(obd_sensors.SENSORS[I].shortname + obd.get_sensor_value(obd_sensors.SENSORS[i]) + obd_sensors.SENSORS[i].unit)
