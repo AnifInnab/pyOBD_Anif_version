b321ffc19d1fbc6d4bd9f357d6de3d1219668744
AnifInnab <anifinnab13@gmail.com>
2015-04-14 02:36:12 +0200

    v.01

--- Rpi_logger.py
+++ Rpi_logger.py
@@ -1,5 +1,14 @@
 import OBD_IO
+import obd_sensors
 
 obd = OBD_IO.OBDPort("/dev/pts/2", 1, 5)
+
+
+obd.send_command("0100")
+
+result = obd.get_result()
+pids = hex_to_bitstring(result)
+
+print(pids)
 
 print("Sucess!")
