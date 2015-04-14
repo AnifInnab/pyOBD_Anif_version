3fb9bcf940dcc92f611bd241fe0e39daae19c7b9
AnifInnab <anifinnab13@gmail.com>
2015-04-14 03:42:13 +0200

    32

--- Rpi_logger.py
+++ Rpi_logger.py
@@ -3,7 +3,7 @@ import obd_sensors
 
 obd = OBD_IO.OBDPort("/dev/pts/1", 1, 5)
 
-for i in range (20):
-    print(obd_sensors.SENSORS[i].shortname + "" + str(obd.get_sensor_value(obd_sensors.SENSORS[i])) + obd_sensors.SENSORS[i].unit)
+for i in range (32):
+    print(obd_sensors.SENSORS[i].shortname + ": " + str(obd.get_sensor_value(obd_sensors.SENSORS[i])) + " " + obd_sensors.SENSORS[i].unit)
 
 print("Sucess!")
