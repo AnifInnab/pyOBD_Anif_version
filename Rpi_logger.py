9efce6faeb8a31d9e4f5a2ffdba69df5c931da27
AnifInnab <anifinnab13@gmail.com>
2015-04-14 03:52:27 +0200

    s

--- Rpi_logger.py
+++ Rpi_logger.py
@@ -1,9 +1,18 @@
 import OBD_IO
 import obd_sensors
-
+import time
+import datetime
 obd = OBD_IO.OBDPort("/dev/pts/1", 1, 5)
+inner = ""
 
-for i in range (32):
-    print(obd_sensors.SENSORS[i].shortname + ": " + str(obd.get_sensor_value(obd_sensors.SENSORS[i])) + " " + obd_sensors.SENSORS[i].unit)
+for i in range (20):
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    seq = ('[TIME, '+ st + ']')
+    inner = ""
 
+    for i in range (32):
+       seq += "[" + obd_sensors.SENSORS[i].shortname + ": " + str(obd.get_sensor_value(obd_sensors.SENSORS[i])) + " " + obd_sensors.SENSORS[i].unit  + "]"
+       print(seq)
+       seq = ""
 print("Sucess!")
