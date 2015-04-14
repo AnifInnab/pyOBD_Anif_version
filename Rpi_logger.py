a8c92b17b4ffbccbf6ea64711cc6315bf76bb289
AnifInnab <anifinnab13@gmail.com>
2015-04-14 02:37:46 +0200

    nvm

--- Rpi_logger.py
+++ Rpi_logger.py
@@ -7,7 +7,7 @@ obd = OBD_IO.OBDPort("/dev/pts/2", 1, 5)
 obd.send_command("0100")
 
 result = obd.get_result()
-pids = hex_to_bitstring(result)
+pids = obd_sensors.hex_to_bitstring(result)
 
 print(pids)
 
