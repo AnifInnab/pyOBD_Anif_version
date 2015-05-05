ec6e8a018908d49519dcfdc2f673bae64211bc82
AnifInnab <anifinnab13@gmail.com>
2015-05-05 13:20:22 +0200

    e

---
+++ launcher.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /
+cd home/pi/pyOBD_Anif_version
+sudo python module2.py
+cd /
