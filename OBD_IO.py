9a04ae8d069bb0dc72fc53c5dc38f3d9ea3d0881
AnifInnab <anifinnab13@gmail.com>
2015-05-08 19:20:22 +0200

    k

--- OBD_IO.py
+++ OBD_IO.py
@@ -70,6 +70,7 @@ class OBDPort:
              
          except serial.SerialException as e:
              print (e)
+             self.__init__(self.portname, 1, 7)
              self.State = 0
              return None
              
