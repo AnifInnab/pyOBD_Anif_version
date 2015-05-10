a4e245d887c42d73fbaeb710bdb19273141c19c3
AnifInnab <anifinnab13@gmail.com>
2015-05-10 18:09:03 +0200

    e

--- OBD_IO.py
+++ OBD_IO.py
@@ -111,6 +111,7 @@ class OBDPort:
          ready = self.get_result()
          
          print("ready: " + ready)
+         print("Read[0]: " + ready[0])
          while(ready[0] is not "1" or ready[0] is not "0" or ready[0] is not "4"):
             print("Reconnecting...")
             self.send_command("0100")
